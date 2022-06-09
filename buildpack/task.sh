c364607ff8ad0a9595bda4383ae8eadf6eacb608
Anirban Das <anirban.das@smarsh.com>
2022-06-09 15:43:54 +0100

    Update auth to dockerhub

--- buildpack/task.sh
+++ buildpack/task.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p /layers 
+mkdir -p /layers
 mkdir -p /platform
 cp -r source /
 
@@ -27,18 +27,21 @@ then
   cp -r bindings /platform/
 fi
 
-export CNB_REGISTRY_AUTH="{\"${IMAGE_REPO}\": \"Basic $(echo -n "${IMAGE_REPO_USERNAME}:${IMAGE_REPO_PASSWORD}" | base64 -w 0)\"}"
+unset CNB_REGISTRY_AUTH
+mkdir ~/.docker
+echo "{\"auths\":{\"${IMAGE_REPO}\":{\"username\":\"${IMAGE_REPO_USERNAME}\",\"password\":\"${IMAGE_REPO_PASSWORD}\"}}}" >> ~/.docker/config.json
+
 /cnb/lifecycle/creator -app=/source \
-    -cache-image=${CACHE_IMAGE} \
-    -cache-dir=${CACHE_DIR} \
+    -cache-image="${CACHE_IMAGE}" \
+    -cache-dir="${CACHE_DIR}" \
     -uid=1000 \
     -gid=1000 \
     -layers=/layers \
     -platform=/platform \
     -report=/layers/report.toml \
-    -process-type=${PROCESS_TYPE} \
-    -skip-restore=${SKIP_RESTORE} \
-    -run-image=${RUN_IMAGE} \
-    ${APP_IMAGE}
+    -process-type="${PROCESS_TYPE}" \
+    -skip-restore="${SKIP_RESTORE}" \
+    -run-image="${RUN_IMAGE}" \
+    "${APP_IMAGE}"
 
 cat /layers/report.toml | grep "digest" | cut -d'"' -f2 | cut -d'"' -f2 | tr -d '\n' | tee image/digest
